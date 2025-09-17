634d868e009b5f9a16a2e4845d5aa2b532b84930
MUKILTEO\1612538 <josesnoisle2@gmail.com>
2025-09-17 13:27:26 -0700

    Sprite walking left/right complete, Sprite image added plus added some walking/idle animations

--- 2D Template/Assets/Platformermovement.cs
+++ 2D Template/Assets/Platformermovement.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Platformermovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+
+    private float _movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb2d.linearVelocityX = _movement;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        _movement = ctx.ReadValue<Vector2>().x * moveSpeed;
     }
 }
