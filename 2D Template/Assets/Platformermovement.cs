16f53a9b533a59af6a0e0ac37c5c61d2ad957ba4
MUKILTEO\1612538 <josesnoisle2@gmail.com>
2025-09-18 13:26:28 -0700

    Animations are mostly done (not yet still buggy), started ui for jump bar

--- 2D Template/Assets/Platformermovement.cs
+++ 2D Template/Assets/Platformermovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,11 +8,13 @@ public class Platformermovement : MonoBehaviour
     public Rigidbody2D rb2d;
 
     private float _movement;
+    private Animator animator; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,6 +25,13 @@ public class Platformermovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext ctx)
     {
+        animator.SetBool("IsWalking", true);
+        if (ctx.canceled) 
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetFloat("lastinputX", _movement);
+        }
         _movement = ctx.ReadValue<Vector2>().x * moveSpeed;
+        animator.SetFloat("InputX", _movement);
     }
 }
