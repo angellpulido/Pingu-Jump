c5255a69bde76e3b5b9dd65717ff6b4d5278057e
MUKILTEO\1725593 <camfry77@gmail.com>
2025-09-23 13:28:06 -0700

    oH YEA

--- 2D Template/Assets/PlayerHealth.cs
+++ 2D Template/Assets/PlayerHealth.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 10;
     public int health;
+    public Slider slider;
 
  
 
@@ -11,15 +14,19 @@ public class PlayerHealth : MonoBehaviour
     void Start()
     {
         health = maxHealth;
-       
+        slider.maxValue = maxHealth;
+        slider.value = health;
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
+        slider.value = health;
+
         if (health <= 0)
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(0);
         }
     }
 }
